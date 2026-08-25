6891bc493f7f7db4384a3aac45a6f18623d1228e
Alexa533 <AgreenBRACELET@gmail.com>
2026-08-25 03:42:47 -0700

    still working on the navigation after submitting form...

--- form-response.js
+++ form-response.js
@@ -1,18 +1,18 @@
-window.addEventListener("load", function() {
-  const form = document.getElementById('my-form');
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const data = new FormData(form);
-    const action = e.target.action;
-    fetch(action, {
-      method: 'POST',
-      body: data,
-    })
-    .then(() => {
-      alert("Success!");
-    })
-  });
-});
+// window.addEventListener("load", function() {
+//   const form = document.getElementById('my-form');
+//   form.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     const data = new FormData(form);
+//     const action = e.target.action;
+//     fetch(action, {
+//       method: 'POST',
+//       body: data,
+//     })
+//     .then(() => {
+//       alert("Success!");
+//     })
+//   });
+// });
 
 // const scriptURL = 'https://script.google.com/macros/s/AKfycbwxaJkKbxAk9FoB1ib4WFCbeOIRNev_D74FIWJ94tpWH_3ksyafh3kP-kuxNANynhJv/exec'
 
